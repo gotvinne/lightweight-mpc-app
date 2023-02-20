205ad7f20a799d63b8c4c6711065fb5366494543
Geir Ola <gotvinne@stud.ntnu.no>
2023-02-20 23:14:35 +0100

    Made react app

---
+++ lightweight-mpcapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Title from "./objects/Title" 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
