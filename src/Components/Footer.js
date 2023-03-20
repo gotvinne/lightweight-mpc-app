f55ab0725b2990303daf6f82352d83b356bb3162
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-03-20 16:44:24 +0100

    Changed design of footer and placed it at the bottom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,20 +2,18 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import CopyrightIcon from '@mui/icons-material/Copyright';
 
+import "../css/Footer.css"
+
 export default function Footer() {
     return (
-        <Box mt={3} position="absolute" bottom="0px" sx={{width: "100%", height: "6%", bgcolor: '#b4b4b4'}}>
-            <Box sx={{pt: 1, display: "flex", flexDirection: "row"}}>
-                <CopyrightIcon sx={{color:"#ff1344", pl: "46%"}} /> 
+        <div className="Footer">
+            <Box sx={{pt: 2, pb: 2, display: "flex", flexDirection: "row"}}>
+                <CopyrightIcon sx={{color:"#ff1344", pl: "40%"}} /> 
                 <Box sx={{pl: 0.5}} />
                 <Typography color="#ff1344" sx={{pt: 0.2, fontWeight: 'bold'}} align="center">
-                    Copyright 2023
+                    2023. Copyright Geir Ola Tvinnereim
                 </Typography>
             </Box>
-            <Box />
-            <Typography color="#ff1344" sx={{pb: 1, fontWeight: 'bold'}} align="center">
-                Geir Ola Tvinnereim
-            </Typography>
-        </Box>
+        </div>
     );
 }
