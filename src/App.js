8c4498b80a346b823f6c99f7e6a9b982f82f192b
Geir Ola Tvinnereim <geir-tvinnereim@hotmail.com>
2023-03-21 19:44:09 +0100

    Imported c functionality to simulation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from "react";
-import createModule from "./web.mjs";
+import React from "react";
 
 import Header from "./Components/Header";
+import Footer from "./Components/Footer"
 import "./css/App.css"
 
 function App() {  
-  const [add, setAdd] = useState();
-  useEffect(  () => {createModule().then((Module) => { 
-    setAdd(() => Module.cwrap("add", "number", ["number", "number"]));
-  });  
-    }, []);  
-
-  if (!add) {  
-    return "Loading webassembly...";  
-  }  return (    
+  return (    
   <div className="App">    
       <Header />
-      <p>Let's do some basic addition:</p>  
-        <div>123 + 234 = {add(123, 1)}</div> 
+      <Footer />
   </div>  
   );
 }
--- src/Components/Simulation.js
+++ src/Components/Simulation.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
+
+import LightWeightMPC from "./../web.mjs";
 
 import "../css/Simulation.css"
 
-
 export default function Simulation() {
-    return (
-        <div className="Simulation">
-            <Box sx={{ width: "100%", pt: 2, pl: 2 }}>
-                
-            </Box>
+    const [add, setAdd] = useState();
+    useEffect(  () => {LightWeightMPC().then((Module) => { 
+    setAdd(() => Module.cwrap("add", "number", ["number", "number"]));
+    });  
+        }, []);  
 
-        </div>
+    if (!add) {  
+        return "Loading webassembly...";  
+    }  return (    
+    <div className="Simulation">
+        <Box sx={{ width: "100%", pt: 2, pl: 2 }}>
+            <p>Let's do some basic addition:</p>  
+            <div> {add(1,2)} </div> 
+        </Box>
+    </div>
     );
 }
